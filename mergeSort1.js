7843c738d1a633f39e4b14fc27e26e1ba30ef931
LindsayElia <lindsay.elia@gmail.com>
2015-05-14 15:53:43 -0700

    added comments to myself for future reference

--- mergeSort1.js
+++ mergeSort1.js
@@ -4,8 +4,8 @@
 // returns one array which contains the sorted numbers
 // from left and right.
 
-
-// TIM'S SOLUTION FROM CLASS:
+// this puts two arrays together by comparing the first item in each array and pushing
+// the smaller of those two items into a new array called 'results'
 exports.merge = function(leftArr, rightArr){
 
 	var results = [];
@@ -13,20 +13,21 @@ exports.merge = function(leftArr, rightArr){
 	var RI = 0;
 
 	while (LI < leftArr.length || RI < rightArr.length){
-		if (LI === leftArr.length) {
-			results.push(rightArr[RI]);
-			RI++;
+		if (LI === leftArr.length) {		// if the LI is at the end of the leftArr length, 
+			results.push(rightArr[RI]);		// that means it will return undefined and
+			RI++;							// this array is empty, so we know that we should just
+											// return whatever is in the right array
 		}
-		else if (RI === rightArr.length) {
-			results.push(leftArr[LI]);
+		else if (RI === rightArr.length) {	// ditto for the right array, if empty, return
+			results.push(leftArr[LI]);		// whatever is in the left array
 			LI++;
 		}
-		else if (leftArr[LI] <= rightArr[RI]) {
-			results.push(leftArr[LI]);
-			LI++;
-		}
+		else if (leftArr[LI] <= rightArr[RI]) {	// if the VALUE that is stored inside of the 
+			results.push(leftArr[LI]);			// left array index is less than or equal to the VALUE
+			LI++;								// that is stored inside of the right array index,
+		}										// increment the index counter
 		else if (leftArr[LI] > rightArr[RI]) {
-			results.push(rightArr[RI]);
+			results.push(rightArr[RI]);			// ditto for the reverse
 			RI++;
 		}
 	}
@@ -36,14 +37,14 @@ exports.merge = function(leftArr, rightArr){
 
 // this has to test arrays with more than one item, otherwise the test would return false?
 exports.mergeSort = function(arr) {				
-		if (arr.length < 2 ) {						// line 1
-			return arr;								// line 2
+		if (arr.length < 2 ) {								// line 1
+			return arr;										// line 2
 		}
 		
 		// we split the array in half:
-		var midway = Math.floor(arr.length/2)			// line 3
+		var midway = Math.floor(arr.length/2)				// line 3
 		var newLeftArr = arr.slice(0, midway);				// line 4
-		var newRightArr = arr.slice (midway, arr.length);		// line 5
+		var newRightArr = arr.slice (midway, arr.length);	// line 5
 		// slice is different than splice!
 		// slice does not extract the second number, rather, everything before and just up to that number
 		
